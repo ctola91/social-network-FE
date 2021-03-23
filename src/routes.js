efa3ced903bf8068fac176bd290f5e493f6fedaa
ctola91 <christian.tola@hotmail.com>
2021-03-23 09:34:43 -0400

    add routes

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import Pomodoro from './components/Pomodoro/Timer';
 import Numbers from './components/Numbers/Numbers';
 import Person from './components/Person/Person';
 import Calculator from './components/Calculator/Calculator';
+import Create from './components/Create';
+import Login from './components/Login';
 import Error404 from './components/Error/404';
 
 const AppRoutes = () => (
@@ -16,6 +18,8 @@ const AppRoutes = () => (
       <Route path="/numbers" component={Numbers} exact />
       <Route path="/person" component={Person} exact />
       <Route path="/calculator" component={Calculator} exact />
+      <Route path="/create" component={Create} exact />
+      <Route path="/login" component={Login} exact />
       <Route component={Error404} />
     </Switch>
   </App>
